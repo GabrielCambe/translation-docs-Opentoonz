a9beddeb185452c55c3807acd540c82fc75f16c5
GabrielCambe <almeidasalesgabriel@gmail.com>
2019-09-27 14:28:31 -0300

    commit com MAKE

---
+++ get_single_folder_from_github.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# navigate to the folder you want to download
+# i.e: https://github.com/opentoonz/opentoonz/tree/master/toonz/sources/translations_docs/english
+# change "tree/master" to "trunk"
+# https://github.com/opentoonz/opentoonz/trunk/toonz/sources/translations_docs/english
+# run the command below
+svn checkout https://github.com/opentoonz/opentoonz/trunk/toonz/sources/translations_docs/english
